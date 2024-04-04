a95ef249bacc567e84de1fb37fa78fca6f9a63e1
csavener <cristian.estrada@savener.es>
2024-04-04 14:17:32 +0200

    wip

--- train.py
+++ train.py
@@ -142,8 +142,7 @@ def get_ds(config):
     # It only has the train split, so we divide it overselves
     ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
     # df_filtrado = df[df['texto'].str.len() <= limite_maximo]
-    print (ds_raw[0]['translation'])
-    print (type(ds_raw[0]['translation']))
+    print (ds_raw[0]['translation'].str.len())
     # Build tokenizers
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
     tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
